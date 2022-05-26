e48b07c741e020b175080edf680969762ee81b45
Dennys Izhyck <irynapistun@gmail.com>
2022-05-26 11:29:18 +0300

    corrected the creation of objects

--- main.py
+++ main.py
@@ -1,22 +1,26 @@
-from models import Employee, Worker, Foreman, Manager, Scientist
+from models.Employee import Employee
+from models.Worker import Worker
+from models.Foreman import Foreman
+from models.Manager import Manager
+from models.Scientist import Scientist
 
 
-employee = Employee.Employee("Anton", 22, 9500)
+employee = Employee("Anton", 22, 9500)
 print(str(employee))
 employee.pay_salary()
 print()
 
-sales_manager = Manager.Manager("Anna", 40, 15000, 10)
+sales_manager = Manager("Anna", 40, 15000, 10)
 print(str(sales_manager))
 print()
 
-inventor = Scientist.Scientist("Andrew", 51, 20000, 8)
+inventor = Scientist("Andrew", 51, 20000, 8)
 print(str(inventor))
 print()
 
-worker = Worker.Worker("Vasyl", 45, 13800, True)
+worker = Worker("Vasyl", 45, 13800, True)
 print(str(worker))
 print()
 
-foreman = Foreman.Foreman("Roman", 37, 17400, 9)
+foreman = Foreman("Roman", 37, 17400, 9)
 print(str(foreman))
